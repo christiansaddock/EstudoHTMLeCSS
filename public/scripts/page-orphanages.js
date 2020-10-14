8bd6a1fc76c8bc8698ab2b2d3c418fc19113dff9
christiansaddock <christian_sa@hotmail.com>
2020-10-14 13:11:13 -0300

    Initial commit

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,28 @@
+
+//cria mapa
+const map = L.map('mapid').setView([-25.4559554,-49.3205856], 15)
+
+//cria e adiciona camadas
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+//cria icone
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
+
+//cria popup
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minHeight:240,
+    minwidth:240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+
+ //adiciona marcador
+L
+    .marker([-25.4559554,-49.3205856], { icon })
+    .addTo(map)
+    .bindPopup(popup)
